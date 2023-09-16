8d75ebff84c4dd8216ea1aa80f275af5f313bd48
trancaothien <thiennh278612@gmail.com>
2023-09-16 16:39:36 +0700

    Initial Commit

---
+++ IOSTemplateNews/IOSTemplateNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTemplateNewsApp.swift
+//  IOSTemplateNews
+//
+//  Created by Trần Cao Thiên on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSTemplateNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
